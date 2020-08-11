6df44677aa3944c651fb284683c348f6113ef683
Jeanine Beckle <jeaninebeckle@gmail.com>
2020-08-10 20:36:04 -0500

    goats cards display

--- src/App/App.js
+++ src/App/App.js
@@ -1,16 +1,31 @@
 import React from 'react';
+import GoatCorral from '../components/GoatCorral/GoatCorral';
 import './App.scss';
+import goatData from '../helpers/data/goatData';
 
 class App extends React.Component {
+  state = {
+    goats: [],
+  }
+
+  componentDidMount() {
+    const goats = goatData.getGoats();
+    this.setState({ goats });
+  }
+
   render() {
+    const { goats } = this.state;
+
     return (
       <div className="App">
-        <h2>INSIDE APP COMPONENT</h2>
-        <button className="btn btn-info">
-          <i className="fab fa-react"></i>I am a button</button>
+        <h1>INSIDE APP COMPONENT</h1>
+        <GoatCorral goats={goats}/>
       </div>
     );
   }
 }
+// first "goats" on line 22 must match word inside brackets on line 5 in GoatCorral.js
 
 export default App;
+
+// apps need a render function and inside, it must return something (jsx)
