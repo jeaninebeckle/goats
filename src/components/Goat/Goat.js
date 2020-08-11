f1e19e45520d15081ca76bba2782c1ddd39e465c
Jeanine Beckle <jeaninebeckle@gmail.com>
2020-08-11 18:45:11 -0500

    add PropTypes shape for goats and use in components

--- src/components/Goat/Goat.js
+++ src/components/Goat/Goat.js
@@ -1,17 +1,22 @@
 import React from 'react';
-import './Goat.scss';
+
+import goatShape from '../../helpers/propTypes/goatShape';
 
 class Goat extends React.Component {
+  static propTypes = {
+    goat: goatShape.goatShape,
+  }
+
   render() {
     const { goat } = this.props;
     return (
-      <div className="card">
-        <img className="card-img-top" src={goat.imgUrl} alt="description" />
-        <div className="card-body">
-          <h5 className="card-title">{goat.name}</h5>
-          <p className="card-text">This goat is {goat.age} years old and is described as {goat.description}</p>
-        </div>
+      <div className="card bg-dark text-light border-0">
+      <img className="card-img-top" src={goat.imgUrl} alt="one specific goat" />
+      <div className="card-img-overlay">
+        <h5 className="card-title text-uppercase">{goat.name}</h5>
+        <p className="card-text">This goat is {goat.age} years old and are described as: {goat.description}</p>
       </div>
+    </div>
     );
   }
 }
